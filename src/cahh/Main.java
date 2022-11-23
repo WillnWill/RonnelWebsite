73adee668bcb25e6ba29dfa9e9581d31f0567f9b
BigCoderRon <80131320+BigCoderRon@users.noreply.github.com>
2022-11-23 21:08:54 +0000

    Update Main.java

--- src/cahh/Main.java
+++ src/cahh/Main.java
@@ -11,5 +11,11 @@ package cahh;
 public class Main {
 
    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
