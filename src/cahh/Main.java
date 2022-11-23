c2e9bc6a62bdc66136723291e441ee57fbdff04a
BigCoderRon <80131320+BigCoderRon@users.noreply.github.com>
2022-11-23 21:08:19 +0000

    cahh.java
    I deleted this cuz i think im cool

--- src/cahh/Main.java
+++ src/cahh/Main.java
@@ -10,11 +10,6 @@ package cahh;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   
     
 }
